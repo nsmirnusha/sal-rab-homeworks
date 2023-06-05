630832e3df197561dcb7b1d267f3f1289718cf47
Neto test <nastya_199208@icloud.com>
2023-06-05 21:55:47 +0300

    task 4 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -5,16 +5,8 @@ function addInCartMessage(title, price) {
     // Задание №1.1. Оповещение о добавлении в корзину
 
     let message;
-    let productName = title;
-    let productPrice = price;
-    message = productName;
-    message += ' ';
-    message += ' за ';
-    message += ' ';
-    message += productPrice;
-    message +=' ';
-    message += ' теперь в корзине!';
-    console.log(message); 
+    message = `${title} за ${price} теперь в корзине!`;
+    console.log(message);
 
     // Конец решения задания №1.1.
 
@@ -25,9 +17,8 @@ function addInCartCountChange(value) {
     let newValue = oldValue
 
     // Задание №1.2. Увеличение количества в корзине
-    let oldValue = value;
-    let newValue = oldValue; 
-    console.log(newValue + 1);
+    let newValue = oldValue + 1;
+    console.log(newValue);
 
     // Конец решения задания №1.2.
 
@@ -40,10 +31,9 @@ function addInCartSumChange(sum, delta) {
 
     // Задание №1.3. Увеличение суммы в корзине
 
-    let newSum;
-    newSum = oldSum + difference;
-    let newSumText = newSum + ' ₽';
-    newSumText = `${newSum} ₽`;
+    let newSum = oldSum + delta;
+    let newSumText = `${newSum} ₽`;
+    console.log(newSumText);
 
     // Конец решения задания №1.3.
 
